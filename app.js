218e3e4bed24f20ca20dce6a738c181d19bc3494
Thakur2001 <76693699+Thakur2001@users.noreply.github.com>
2023-02-15 13:06:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var endDate="15 Febuary 2023 12:52 PM";
+document.getElementById("end-date").innerText = endDate;
+const input = document.querySelectorAll("input")
+function clock(){
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff =(end-now)/1000;
+    if(diff<0)return;
+    input[0].value=(Math.floor(diff/3600/24));
+    input[1].value=Math.floor(diff/3600)%24;
+    input[2].value=Math.floor(diff/60)%60;
+    input[3].value=Math.floor(diff%60);
+}
+clock()
+setInterval(() => {
+    clock()
+    
+}, 1000);
